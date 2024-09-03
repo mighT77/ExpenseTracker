b1769eb69a6834da66932140d999b2de647ade99
mighT77 <142781273+mighT77@users.noreply.github.com>
2024-09-03 18:19:54 +0800

    Initializing models and DB setup

--- ExpenseTracker/Program.cs
+++ ExpenseTracker/Program.cs
@@ -1,7 +1,13 @@
+using ExpenseTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//DI
+builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
 
 var app = builder.Build();
 
