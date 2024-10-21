bf1602c7363b86c42294a917064a95ce1767551a
mighT77 <142781273+mighT77@users.noreply.github.com>
2024-10-21 21:40:50 +0800

    Added transactions of last 7 days

--- ExpenseTracker/Controllers/DashboardController.cs
+++ ExpenseTracker/Controllers/DashboardController.cs
@@ -1,11 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ExpenseTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Transactions;
 
 namespace ExpenseTracker.Controllers
 {
     public class DashboardController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDBContext _context;
+
+        public DashboardController(ApplicationDBContext context)
+        { 
+            _context = context;
+        }
+        public async IActionResult Index()
         {
+            //last 7 days transactions
+            DateTime StartDate = DateTime.Today.AddDays(-6);
+            DateTime EndDate = DateTime.Today;
+
+            List<Transaction> SelectedTransactions = await _context.Transactions
+
             return View();
         }
     }
